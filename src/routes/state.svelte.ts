24fcfc30264360f4f7f0aae2659d8d8b7cf49650
Éverton Toffanetto <evertondgn@hotmail.com>
2025-02-26 02:34:50 -0300

    feat(state): add new state variable count2 and corresponding getter

--- src/routes/state.svelte.ts
+++ src/routes/state.svelte.ts
@@ -1,9 +1,14 @@
 let count = $state(0)
+const count2 = $state(['abc'])
 const doubleCount = $derived(count * 2)
 
 export const counterState = {
   get getCount() {
     return count
+  },
+
+  get getCount2(): readonly string[] {
+    return count2
   },
 
   get getDoubleCount() {
