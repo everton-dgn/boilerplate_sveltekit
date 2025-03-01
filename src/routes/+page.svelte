478d2c819b12434f2b1476108468c267d9142668
Éverton Toffanetto <evertondgn@hotmail.com>
2025-03-01 04:28:33 -0300

    feat(page): add meta tags for SEO and update header locator in tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
   import S from './styles.module.css'
 </script>
 
+<svelte:head>
+  <title>Boilerplate</title>
+  <meta name="description" content="Boilerplate Sveltekit" />
+</svelte:head>
+
 <div class={S.container}>
   <header class={S.header}>Boilerplate</header>
   <main class={S.main}>
