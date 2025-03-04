d0b6da0e9bbf8e6766da8bde7ed561ee5ecff80a
Éverton Toffanetto <evertondgn@hotmail.com>
2025-03-04 03:52:57 -0300

    feat(styles): update Inter font imports to include Latin Extended variants

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import '@fontsource/inter/400.css'
-  import '@fontsource/inter/500.css'
-  import '@fontsource/inter/600.css'
+  import '@fontsource/inter/latin-ext-400.css'
+  import '@fontsource/inter/latin-ext-500.css'
+  import '@fontsource/inter/latin-ext-600.css'
   import '@/theme/reset.css'
 
   import MainProvider from '@/components/providers/MainProvider.svelte'
