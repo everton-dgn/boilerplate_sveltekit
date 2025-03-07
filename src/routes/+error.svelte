7264bd55da2ab22ac80abc7d691088d5f8438af7
Éverton Toffanetto <evertondgn@hotmail.com>
2025-03-07 01:15:58 -0300

    feat(error): add error handling page and layout server function

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from '$app/state'
+</script>
+
+<svelte:head>
+  <title>Error</title>
+</svelte:head>
+
+<h1>{page.status} {page.error?.message ?? 'Try again!'}</h1>
